8d529586017742d1a65222e1f1e142fca64ae988
Mantostradale <davide.mantovan2005@gmail.com>
2026-06-11 18:43:34 +0200

    fix EasiFragment.kt

--- app/src/main/java/com/example/dermcalc/EasiFragment.kt
+++ app/src/main/java/com/example/dermcalc/EasiFragment.kt
@@ -114,8 +114,8 @@ class EasiFragment : Fragment(R.layout.fragment_index_easi) {
             txtEasiResultScore.text = punteggioFinaleCalcolato.toString()
             txtEasiResultSeverity.text = when {
                 totaleEasi == 0.0 -> "Assente"
-                totaleEasi <= 1.0 -> "Lieve"
-                totaleEasi <= 7.0 -> "Moderato"
+                totaleEasi < 5 -> "Lieve"
+                totaleEasi <= 10 -> "Moderato"
                 else -> "Grave"
             }
         }
@@ -213,12 +213,12 @@ class EasiFragment : Fragment(R.layout.fragment_index_easi) {
 
     private fun convertiAreaMoltiplicatore(position: Int): Double {
         return when (position) {
-            1 -> 0.1
-            2 -> 0.2
-            3 -> 0.3
-            4 -> 0.4
-            5 -> 0.5
-            6 -> 0.6
+            1 -> 1.0
+            2 -> 2.0
+            3 -> 3.0
+            4 -> 4.0
+            5 -> 5.0
+            6 -> 6.0
             else -> 0.0
         }
     }
